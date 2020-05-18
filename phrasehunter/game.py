996c25a81137cc82d87d0cd9ba0ea65506ab9fb7
javila21 <jerry.avila21@gmail.com>
2020-05-18 19:40:07 -0400

    added dict notation

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,56 +1,67 @@
-# Create your Game class logic in here.
-import os 
 import random
 import sys
 from phrase import Phrase
 from phrase import consol_output
 from character import Character
-
+"""List of phrases to be guessed in game"""
 phrase_list = ['keep your chin up', 'wild at heart', 'hello world', 'embrace the journey', 'be the change', 'focus and win', 'do it now', 'never give up']
+"""Sets lives for wrong guesses in game"""
 total_lives = 5
+"""Shuffles phrase_list for first run"""
 random.shuffle(phrase_list)
 class Game():
     
     def __init__(self, whole_phrase=False,*args, **kwargs): 
         self.consol_output = consol_output
+        """Sets the phrase for the games"""
         self.phrase = phrase_list[1]
         self.player_guess = None
         self.welcome_message()
         self.start_game()
         
     
-    
+    """Starts Game"""
     def start_game(self):
         Phrase(phrase_list[1],self.player_guess, new_game=False, run_extend=True).charac_in_phrase()
+        """Shuffles phrase_list for additional playes after first run"""
         random.shuffle(phrase_list)
+        """Sets the number of guesses"""
         used_lives = total_lives
+        """Game loop"""
         while True:
             
             self.phrase_output(self.phrase)
+            """If no "_" in output then game is won"""
             if "_" not in self.consol_output:
                 self.you_won()   
             self.guess_input()
-            true_guess = Character(self.player_guess, self.phrase, life_check=True)
+            """Checks if guess input matches phrase in Character file and then counts down if                not correct
+"""
+            true_guess = Character(self.player_guess, self.phrase)
             if true_guess.was_guessed is False:
                 used_lives -= 1
+                """If all 5 lives are used then end game"""
             if used_lives <= 0 :
                 self.better_luck()
+                """Output how many lives are left"""
             print('You have {} out of {} lives remaining!'.format(used_lives,       total_lives))
-        
+    """Welcome message Method"""    
     def welcome_message(self):
         print("Welcome to Phrase Hunter!")
         print("-----------------------------") 
         print("You get 5 lives, to guess the phrase.""\n""For each incorrect guess 1 life is lost. Good Luck!!!""\n")
-    
+        
+    """Output the phrase to be guessed against and adds correct guess to output"""
     def phrase_output(self, phrase):
         self.phrase = phrase
         Phrase(phrase, self.player_guess, run_extend=False).charac_in_phrase()
         print('{}'.format(''.join(self.consol_output)))
         
+    """Input request for player with expections"""    
     def guess_input(self):
         try:
             self.player_guess = input('Guess a letter: ')
-            Character(self.player_guess, self.phrase, life_check=False)
+            Character(self.player_guess, self.phrase)
         except ValueError: 
             print("That was not a valid input. Please pick a number between 1 and 10")
         if self.player_guess == "":
@@ -59,15 +70,18 @@ class Game():
             print ("Please only enter a letters,try again.")
         if len(self.player_guess) > 1:
             print("Please enter only one letter at a time.")
-        
+            
+    """Winning Message"""    
     def you_won(self):
         print("Congratualtions you won the Game!!!!")
         self.end_of_game()
         
+    """Losing Message"""    
     def better_luck(self):
         print("Better luck next time.")
         self.end_of_game()
         
+    """End of Game Method with option to exit or play again"""    
     def end_of_game(self):
         try:
             play_again = input("Would you like to play again?[y]es/[n]o: ")
@@ -85,10 +99,3 @@ class Game():
             print("That is is not a valid value please use either y or n.")
             self.end_of_game()
         
-#Create the Game class in the game.py file
-#The class should include an initializer or def __init__ method that receives a phrases parameter and holds these phrases in an instance attribute on the game object.
-
-#You will need at least 1 instance attribute to start the game:
-
-#An instance attribute that stores the current Phrase object and starts the game. You may think of this as the Active phrase being guessed against by the player while the game is running.
-#The Game instance might be responsible for things like: starting the game loop, getting player's input() guesses to pass to a Phrase object to perform its responsibilities against, determining if a win/loss happens after the player runs out of turns or the phrase is completely guessed.
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,10 +1,11 @@
-# Create your Phrase class logic here.
 import re
 import copy
 from character import Character
 from character import correct_guesses
 
+"""A list of the phrase where each character is a string"""
 character_list = []
+"""The phrase that the player sees"""
 consol_output = []
 
 class Phrase():
@@ -14,34 +15,25 @@ class Phrase():
         self.run_extend = run_extend
         self.correct_guesses = correct_guesses
         self.player_guess=player_guess
+        """If a 2nd game or any game after that is played lists are cleared"""
         if new_game is True:
             character_list.clear()
             consol_output.clear()
         print(self.phrase)
-               
+    """Turns the phrase into a list characters"""           
     def charac_in_phrase(self):
         character_list = list(self.phrase)
-#        print(character_list)
         self.consol_output(character_list)
         
+    """Generates the initial output to be displayed to player"""   
     def consol_output(self, character_list):
-#        print(self.run_extend)
         if self.run_extend is True:
             consol_output.extend(copy.deepcopy(character_list)) 
             for n, char in enumerate(consol_output):
                 if char != " ":
                     consol_output[n] = "_"
-                    
+        """adds corrects guesses to display output"""            
         for n, char in enumerate(character_list):
             if char is self.player_guess:
                 consol_output[n] = self.player_guess
-        
                 
-#Create the Phrase class in the phrase.py file
-#The class should include an initializer or def __init__ that receives a phrase parameter and holds this phrase in an instance attribute on the Phrase object. A phrase should be a collection of Character objects, where each letter of the phrase is a Character() instance stored inside a collection such as a List.
-
-#Any additional instance attributes you feel you need to store are up to you.
-
-#The Phrase instance might be responsible for things like: Knowing if the entire phrase has been guessed, Iterating over its collection of Character to allow a guess to be checked using a Character instance method call or to ask the Character object how it should show its letter.
-
-#The instance method names and their implementation are up to you to determine.
